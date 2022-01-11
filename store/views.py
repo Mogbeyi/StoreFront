2781298814a50596e611e1cd0dab4e9bfc6657ba
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2022-01-11 16:00:34 +0100

    Use generic api views

--- store/views.py
+++ store/views.py
@@ -6,20 +6,13 @@ from .serializers import CollectionSerializer, ProductSerializer
 from .models import Collection, Product
 from rest_framework import status
 from django.db.models import Count
+from rest_framework.generics import ListCreateAPIView
+class ProductList(ListCreateAPIView):
+    queryset = Product.objects.select_related("collection").all()
+    serializer_class = ProductSerializer 
 
-class ProductList(APIView):
-
-    def get(self, request):
-        queryset = Product.objects.select_related("collection").all()
-        serializer = ProductSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = ProductSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
+    def get_serializer_context(self):
+        return {'request': self.request}
 
 class ProductDetail(APIView):
 
@@ -45,20 +38,10 @@ class ProductDetail(APIView):
             )
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
- 
-@api_view(["GET", "POST"])
-def collection_list(request):
-    if request.method == "GET":
-        collections = Collection.objects.annotate(
-            products_count=Count("products")
-        ).all()
-        serializer = CollectionSerializer(collections, many=True)
-        return Response(serializer.data)
-    elif request.method == "POST":
-        serializer = CollectionSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class CollectionList(ListCreateAPIView):
+    queryset = Collection.objects.annotate(products_count=Count("products")).all()
+    serializer_class = CollectionSerializer
 
 
 @api_view(["GET", "PUT", "DELETE"])
