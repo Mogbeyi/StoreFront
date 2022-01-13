19bc17f45115ea941c1118c5969d700f3919eaec
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2022-01-13 17:26:55 +0100

    Implement generic filtering

--- store/views.py
+++ store/views.py
@@ -5,17 +5,14 @@ from .models import Collection, Product, OrderItem, Review
 from rest_framework import status
 from django.db.models import Count
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+from .filters import ProductFilter
 class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProductFilter
 
-    def get_queryset(self):
-        queryset = Product.objects.all()
-        collection_id = self.request.query_params.get('collection_id')
-
-        if collection_id is not None:
-            queryset = queryset.filter(collection_id=collection_id)
-
-        return queryset
 
     def get_serializer_context(self):
         return {"request": self.request}
